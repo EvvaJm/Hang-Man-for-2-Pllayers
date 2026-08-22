69a5a9d40ae3f04e4e163d2c5b71001f8a4aad3b
EvvaJm <evva.morrissey@gmail.com>
2026-08-21 23:02:19 -0400

    Implement player name input and word entry
    Added functionality to input player names and a word for the game.

--- hangman.c
+++ hangman.c
@@ -1,1 +1,24 @@
+#include <stdio.h> 
+#include <stdlib.h>
 
+int main (void) {
+
+char playerOne[50];
+char playerTwo[50];
+char word[50];
+
+printf("What is Player 1's name?\n");
+scanf ("%s", playerOne);
+printf("What is Player 2's name?\n");
+scanf("%s", playerTwo);
+
+printf("Please pass the computer to %s\n", playerOne);
+printf("Please Enter your word: ");
+scanf("%s", word);
+system("cls");
+  
+printf("%s", word);
+
+  return 0;
+
+}
