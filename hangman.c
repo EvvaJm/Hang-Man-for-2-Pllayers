c35dc98aa7465e4d51dc50adbb75b789ad6f9977
EvvaJm <evva.morrissey@gmail.com>
2026-09-03 20:01:40 -0400

    Refactor hangman game for improved readability
    Refactor hangman game logic to streamline player input and guessing process.

--- hangman.c
+++ hangman.c
@@ -1,50 +1,68 @@
 #include <stdio.h> 
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <string.h> 
+ 
+int main (void) { 
+ 
+char playerOne[50]; 
+char playerTwo[50]; 
+char word[50]; 
+char guessedWord[50]; 
+char guessedLetter; 
+int numOfGuessedLetters = 0; 
+ 
+printf("What is Player 1's name?\n"); 
+scanf ("%s", playerOne); 
+printf("What is Player 2's name?\n"); 
+scanf("%s", playerTwo); 
+ 
+printf("Please pass the computer to %s\n", playerOne); 
+printf("Please Enter your word: "); 
+scanf("%s", word); 
+system("cls"); 
+   
+printf("Pass the computer to %s \n", playerTwo); 
+printf ("Ready to start!!\n"); 
+printf("secret word: "); 
+   
+for (int i = 0; word[i] != '\0'; i++){ 
+  guessedWord[i] = '_';
+  printf("_ "); 
+} 
+ 
+guessedWord[strlen(word)] = '\0';
 
-int main (void) {
-
-char playerOne[50];
-char playerTwo[50];
-char word[50];
-char guessedLetter;
-
-printf("What is Player 1's name?\n");
-scanf ("%s", playerOne);
-printf("What is Player 2's name?\n");
-scanf("%s", playerTwo);
-
-printf("Please pass the computer to %s\n", playerOne);
-printf("Please Enter your word: ");
-scanf("%s", word);
-system("cls");
-  
-printf("Pass the computer to %s \n", playerTwo);
-printf ("Ready to start!!\n");
-printf("secret word: ");
-  
-for (int i = 0; word[i] != '\0'; i++){
-printf("_ ");
+printf("\n"); 
+   
+while (numOfGuessedLetters < strlen(word)) {
+    printf("Current Word: ");
+    for (int i = 0; word[i] != '\0'; i++){ 
+      printf("%c ", guessedWord[i]); 
 }
 
-printf("\n");
-  
-printf("%s please take your guess: ", playerTwo);
-scanf(" %c", &guessedLetter);
+  printf("\n");
 
-while (strlen (guessedLetter) != 1){
- printf("ERROR please enter one letter: ");
-scanf(" %c", &guessedLetter);
+  printf("%s please take your guess: ", playerTwo); 
+  scanf(" %c", &guessedLetter); 
+
+  int correctGuess = 0;
+
+  for (int i = 0; word[i] != '\0'; i++){ 
+
+  if (guessedLetter == word[i] && guessedWord[i] == '_'){ 
+  guessedWord[i] = guessedLetter;
+  numOfGuessedLetters += 1;
+  correctGuess = 1;
+  } 
 }
-  
-for (int i = 0; word[i] != '\0'; i++){
-   if (guessedLetter == word[i]){
-     printf("%c ", guessedLetter);
-}else{
-     printf("_ ");
-   }
 
+  if (correctGuess == 1){
+    printf("Correct guess!\n");
+  }else{
+    printf("Incorrect guess!\n");
+  }
 }
-  return 0;
-
-
+printf("Congratulations %s! You guessed the word: %s\n", playerTwo, guessedWord);
+return 0; 
+ 
 }
